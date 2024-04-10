6b855614c5d223f273590ea30238143565124a3c
poche <poche@LAPTOP-J04COSQ8>
2024-04-10 18:22:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string word = "козел";
+    std::string input;
+    std::cin >> input;
+
+    std::cout << "\"" << input << "\" - " << word << std::endl;
+
+    return 0;
+}
